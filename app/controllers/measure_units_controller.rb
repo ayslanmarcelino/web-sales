d0f04651031d36f8d5f122bcee39c47e810a8f0f
ayslanmarcelino <ayslanmarcelino@gmail.com>
2024-04-21 12:25:23 -0300

    feat: add new and create to controller=

--- app/controllers/measure_units_controller.rb
+++ app/controllers/measure_units_controller.rb
@@ -9,4 +9,26 @@ class MeasureUnitsController < ApplicationController
       accessible_by: current_ability
     )
   end
+
+  def new
+    @measure_unit = MeasureUnit.new
+  end
+
+  def create
+    @measure_unit = MeasureUnit.new(measure_unit_params)
+
+    if @measure_unit.save
+      custom_redirect(path: measure_units_path, type: :success)
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def measure_unit_params
+    params.require(:measure_unit)
+          .permit(:description, :abbreviation)
+          .merge(enterprise: current_user.current_enterprise)
+  end
 end
